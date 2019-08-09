85938802f37c182dd481cb9d576893d0f959ddc7
1902573975@qq.com <20150522yang>
2019-08-09 14:25:42 +0800

    report

--- testCases/MultiWindowDemo.py
+++ testCases/MultiWindowDemo.py
@@ -1,37 +1,67 @@
 #coding=utf-8
 
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
 from  multiprocessing import Pool
 from selenium import webdriver
 import  time
 
 
-chromeOpt = Options()
-#chromeOpt.add_argument("--headless")
-#chromeOpt.add_argument("--disable-gpu")
-#chromeOpt.add_argument("--window-size=1400,3600")
-chrome = webdriver.Chrome(chrome_options=chromeOpt)
 
 def openUrl(url):
+    chromeOptNew = Options()
+    chromeOptNew.add_argument("--headless")
+    chromeOptNew.add_argument("--disable-gpu")
+    chromeOptNew.add_argument("--window-size=1400,3600")
+    chromeDriverNew = webdriver.Chrome(chrome_options=chromeOptNew)
     try:
-        windowUrl = "window.open('{}')".format(url)
-        chrome.execute_script(windowUrl)
-        time.sleep(5)
+        #打开新标签推荐使用这种方式
+        # windowUrl = "window.open('{}')".format(url)
+        # chrome.execute_script(windowUrl)
+        # current_page_handle = chrome.window_handles[-1]
+
+        # 缺换handle
+        # print(current_page_handle)
+        # chrome.switch_to.window(current_page_handle)
+
+        chromeDriverNew.get(url)
+        time.sleep(15)
+        content_list = chromeDriverNew.find_elements_by_xpath("//table")
+        for t in content_list:
+            try:
+                aObj = t.find_element_by_tag_name("a")
+                print(aObj.text)
+            except AttributeError as attr:
+                print(attr)
+                pass
+            except NoSuchElementException as nse:
+                print(nse)
+                pass
+            finally:
+                pass
         pass
     except BaseException as e:
         print(e)
+        pass
+    finally:
+        chromeDriverNew.quit()
     pass
 
 
 
 if __name__ == '__main__':
+    chromeOpt = Options()
+    chromeOpt.add_argument("--headless")
+    chromeOpt.add_argument("--disable-gpu")
+    chromeOpt.add_argument("--window-size=1400,3600")
+    chromeDriver = webdriver.Chrome(chrome_options=chromeOpt)
     try:
-        chrome.implicitly_wait(5)  # 隐式等待 5 min
-        chrome.get("https://www.dytt8.net")
-        chrome.find_element_by_id("cs_ap_8040").click()
-        chrome.switch_to.window(chrome.window_handles[0])
+        chromeDriver.implicitly_wait(5)  # 隐式等待 5 min
+        chromeDriver.get("https://www.dytt8.net")
+        chromeDriver.find_element_by_id("cs_ap_8040").click()
+        chromeDriver.switch_to.window(chromeDriver.window_handles[0])
 
-        aListLength = len(chrome.find_elements_by_xpath("//div[@class='co_content2']//a"))
+        aListLength = len(chromeDriver.find_elements_by_xpath("//div[@class='co_content2']//a"))
         # 类型必须是一致的才能使用+
         print("length=" + str(aListLength))
 
@@ -41,7 +71,7 @@ if __name__ == '__main__':
         while initI < 10:
             initI += 1  #自增
             tempXpath = "//div[@class='co_content2']//a[" + str(initI) + "]"
-            href = chrome.find_element_by_xpath(tempXpath).get_attribute("href")
+            href = chromeDriver.find_element_by_xpath(tempXpath).get_attribute("href")
             print(href)
             # 判断href 是否是 None
             if not(href is None):
@@ -52,6 +82,7 @@ if __name__ == '__main__':
         rl  = pool.map(openUrl,urlList)
         pool.close()
         pool.join()
+        print("join")
         pass
     except TypeError as e:
         print(e)
@@ -59,4 +90,4 @@ if __name__ == '__main__':
     except:
         print("error")
     finally:
-        chrome.quit()
+        chromeDriver.quit()
